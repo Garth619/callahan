066a9ad55261d9e6e1f31d4a706b611c919a7db1
Garrett Cullen <Garth619@users.noreply.github.com>
2019-11-20 12:36:03 -0800

    push

--- wp-content/themes/callahan/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/callahan/page-templates/homepage_template_parts/section-1.php
@@ -1,6 +1,16 @@
 <section id="section_one">
 	
-		
+	<div class="sec_one_bg_wrapper">
+	
+		<div class="sec_one_left"></div><!-- sec_one_left -->
+	
+		<div class="sec_one_right">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/hero-desktop.jpg"/>
+			
+		</div><!-- sec_one_right -->
+	
+	</div><!-- sec_one_bg_wrapper -->
 
 	
 </section><!-- section_one -->
