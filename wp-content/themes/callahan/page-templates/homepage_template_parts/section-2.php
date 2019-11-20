ecf0fed6f5f670d62868cfbb324f877cb64fabc2
Garrett Cullen <Garth619@users.noreply.github.com>
2019-11-20 14:01:53 -0800

    push

---
+++ wp-content/themes/callahan/page-templates/homepage_template_parts/section-2.php
@@ -0,0 +1,12 @@
+<section id="section_two">
+	
+	<div class="sec_two_slider">
+		
+		<div class="sec_two_slide">
+
+			
+		</div><!-- sec_two_slide -->
+		
+	</div><!-- sec_two_slider -->
+	
+</section><!-- section_two -->
