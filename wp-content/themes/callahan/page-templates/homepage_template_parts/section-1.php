35545e78fbf4f0b988ee78104291e4bfa48eeac8
Garrett Cullen <Garth619@users.noreply.github.com>
2019-11-20 12:50:53 -0800

    push

--- wp-content/themes/callahan/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/callahan/page-templates/homepage_template_parts/section-1.php
@@ -11,7 +11,32 @@
 		</div><!-- sec_one_right -->
 	
 	</div><!-- sec_one_bg_wrapper -->
-
+	
+	<div class="sec_one_content">
+		
+		<div class="sec_one_content_inner">
+			
+			<div class="sec_one_vid_wrapper">
+				
+				<div class="sec_one_video_thumb_wrapper">
+					
+					<?php echo file_get_contents("wp-content/themes/callahan/images/icon-play-button.svg"); ?>
+					
+					<img class="video_thumb" src="<?php bloginfo('template_directory');?>/images/hero-video-thumb.jpg"/>
+					
+				</div><!-- sec_one_video_thumb_wrapper -->
+				
+			</div><!-- sec_one_vid_wrapper -->
+			
+			<div class="sec_one_title_wrapper">
+				
+				
+				
+			</div><!-- sec_one_title_wrapper -->
+			
+		</div><!-- sec_one_content_inner -->
+		
+	</div><!-- sec_one_content -->
 	
 </section><!-- section_one -->
 
