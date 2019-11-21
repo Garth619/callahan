e2f4089975b369444dee812a937576339080fe11
Garrett Cullen <Garth619@users.noreply.github.com>
2019-11-21 13:47:39 -0800

    push

--- wp-content/themes/callahan/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/callahan/page-templates/homepage_template_parts/section-2.php
@@ -56,8 +56,64 @@
 			
 		</div><!-- sec_two_slide -->
 		
-		
-		
 	</div><!-- sec_two_slider -->
+	
+	<div class="sec_two_content_wrapper">
+		
+		<div class="sec_two_content_intro">
+			
+			<h1>World Class Trial Lawyers</h1>
+			
+			<p>Our law firm represents corporate, professional and entrepreneurial clients of all sizes: multi-national, Fortune 500 companies, California-based corporations, medium and small businesses, homeowners associations, and employers and individuals engaged in business. </p>
+			
+		</div><!-- sec_two_content_intro -->
+		
+		<div class="sec_two_columns content">
+			
+			<!-- css columns get buggy, here are two div columns instead -->
+			
+			<div class="sec_two_col">
+				
+				<p>Callahan & Blaine is California's Premier Litigation Firm. Callahan & Blaine is a 28 attorney litigation firm based in Santa Ana, California. Established in 1984, Callahan & Blaine specializes in complex litigation of all types, including catastrophic personal injury, insurance bad faith and complex business litigation, predominantly on the plaintiff's side.</p>
+
+				<p>Callahan & Blaine has had unparalleled success in obtaining record-breaking verdicts and settlements in diverse and wide-ranging areas of litigation. Callahan & Blaine is recognized nationally for winning the most complex and difficult cases.</p>
+
+
+				<h2>Leadership of Callahan & Blaine</h2>
+				
+				<p><strong>Callahan & Blaine is led by its managing partner Daniel J. Callahan. Dan Callahan is known and respected as one of the top trial lawyers in the United States. Dan's honors and accomplishments include the following:</strong></p>
+				
+				<ul>
+					<li>Dan has been named one of the Top 10 Attorneys in the United States by the National Law Journal.</li>
+					<li>Dan has been named a California Lawyer of The Year by California Lawyer magazine.</li>
+					<li>Dan has been featured in Best Lawyers of America from 2005 to 2019.</li>
+					<li>Dan has been honored as one of the Top 500 Attorneys in the United States by the prestigious Law Dragon 500 publication.</li>
+					<li>Dan has been named Business Litigation Trial Attorney of the Year by the Orange County Trial Lawyers Association on several occasions.</li>
+					<li>Dan is a frequent speaker and author. He has given hundreds of seminars and speeches and written over 50 articles on numerous trial and litigation topics.</li>
+				</ul>
+				
+			</div><!-- sec_two_col -->
+			
+			<div class="sec_two_col">
+				
+				<ul>
+					<li>Dan is featured in Super Lawyers, Outstanding Lawyers of America, Pre-Eminent Lawyers of America and many other publications as one of the premier trial attorneys in the United States. Dan has been named as one of the Top 100 Super Lawyers in Southern California for every year since 2010.</li>
+				</ul>
+
+				
+				<p>Callahan & Blaine also features numerous other highly successful litigation attorneys, including Edward Susolik. Ed, who is head of Callahan & Blaine's Insurance Department, specializes in complex business litigation, severe personal injury and the resolution of complex insurance disputes. Among numerous other awards and accomplishments, Ed has been named one of the Top 100 Super Lawyers in Southern California every year since 2010.</p>
+
+				<h2>History of the firm</h2>
+
+				<p>Established in 1984, the civil litigation firm of Callahan & Blaine concentrated on commercial and corporate trial work until we started taking personal injury cases about twelve years ago. Since that time, our lawyers have obtained two of the largest personal injury settlements in United States history, and we have won many multimillion-dollar recoveries for personal injury and wrongful death clients. Contact our office in Santa Ana to learn how our litigation methods have made us California's premier litigation firm for personal injury claims.</p>
+
+				<p>Callahan & Blaine has been built to achieve outstanding results in civil litigation, no matter what the issue in dispute might happen to be. A good example of the way our complex litigation experience supports our success in personal injury was a catastrophic accident case that we settled for $50 million, the largest reported personal injury settlement in United States history.</p>
+				
+				
+			</div><!-- sec_two_col -->
+			
+		</div><!-- sec_two_columns -->
+		
+	</div><!-- sec_two_content_wrapper -->
 	
 </section><!-- section_two -->
