7ebe181f116c5abcab54fdfe8641a21fce251c37
Garrett Cullen <Garth619@users.noreply.github.com>
2019-11-20 16:03:16 -0800

    push

--- wp-content/themes/callahan/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/callahan/page-templates/homepage_template_parts/section-2.php
@@ -3,19 +3,60 @@
 	<div class="sec_two_slider">
 		
 		<div class="sec_two_slide">
-
+			
+			<a href="">
+		
+				<div class="sec_two_inner">
+				
+					<span class="sec_two_slide_title">Experience</span><!-- sec_two_slide_title -->
+				
+					<span class="sec_two_slide_content">Together, our attorneys have more than 700 years of trial experience, and since 2003, our verdicts and settlements add up to over $1.0 billion. </span><!-- sec_two_slide_content -->
+				
+					<span class="learn_more">Learn More</span><!-- learn_more -->
+				
+				</div><!-- sec_two_inner -->
+			
+			</a>
 			
 		</div><!-- sec_two_slide -->
 		
 		<div class="sec_two_slide">
-
+			
+			<a href="">
+		
+				<div class="sec_two_inner">
+				
+					<span class="sec_two_slide_title">Reputation</span><!-- sec_two_slide_title -->
+				
+					<span class="sec_two_slide_content">Most of our attorneys have won national or regional recognition as effective advocates on complex or novel legal problems from intellectual property to insurance coverage.</span><!-- sec_two_slide_content -->
+				
+					<span class="learn_more">Learn More</span><!-- learn_more -->
+				
+				</div><!-- sec_two_inner -->
+			
+			</a>
 			
 		</div><!-- sec_two_slide -->
 		
 		<div class="sec_two_slide">
-
+			
+			<a href="">
+		
+				<div class="sec_two_inner">
+				
+					<span class="sec_two_slide_title">Practical Skills</span><!-- sec_two_slide_title -->
+				
+					<span class="sec_two_slide_content">Handling evidence on disputed facts under pressure is probably the trial lawyer's core skill, and our lawyers do it magnificently. </span><!-- sec_two_slide_content -->
+				
+					<span class="learn_more">Learn More</span><!-- learn_more -->
+				
+				</div><!-- sec_two_inner -->
+			
+			</a>
 			
 		</div><!-- sec_two_slide -->
+		
+		
 		
 	</div><!-- sec_two_slider -->
 	
