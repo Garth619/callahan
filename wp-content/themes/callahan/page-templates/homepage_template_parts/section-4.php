cfc8e10217e4f56866b6221993596f44a983bbfe
Garrett Cullen <Garth619@users.noreply.github.com>
2019-11-22 12:57:06 -0800

    push

--- wp-content/themes/callahan/page-templates/homepage_template_parts/section-4.php
+++ wp-content/themes/callahan/page-templates/homepage_template_parts/section-4.php
@@ -34,9 +34,9 @@
 				
 				<div class="sec_four_slide_inner">
 					
-					<span class="amount">$47m</span>
+					<span class="amount">$934m</span>
 					
-					<span class="description">Jury clears widow in any wrongdoing in Ponzi Scheme</span><!-- description -->
+					<span class="description">Trial of complex business dispute largest jury verdict in Orange County history</span><!-- description -->
 					
 				</div><!-- sec_four_slide_inner -->
 				
@@ -46,9 +46,9 @@
 				
 				<div class="sec_four_slide_inner">
 					
-					<span class="amount">$47m</span>
+					<span class="amount">$45m</span>
 					
-					<span class="description">Jury clears widow in any wrongdoing in Ponzi Scheme</span><!-- description -->
+					<span class="description">Fortune 500 Company, Mitsubishi Caterpillar Forklift America, Inc., was led to victory</span><!-- description -->
 					
 				</div><!-- sec_four_slide_inner -->
 				
@@ -72,7 +72,49 @@
 	
 	<div class="sec_four_selling_points">
 		
-		<div class="sec_four_selling_point"></div><!-- sec_four_selling_point -->
+		<div class="sec_four_selling_point">
+			
+			<span class="number">01</span>
+			
+			<div class="sec_four_sp_inner">
+				
+				<div class="sec_four_sp_left">
+					
+					<span>We’re not a traditional law firm</span>
+					
+				</div><!-- sec_four_sp_left -->
+				
+				<div class="sec_four_sp_right content">
+					
+					<p>We don't operate like many other firms our size. We don't hire recent law school graduates. Rather, every one of our attorneys joined Callahan & Blaine as an experienced and successful trial practitioner. They are then trained and molded for several years into the Callahan & Blaine litigation style: aggressive, thorough, comprehensive and, most of all, WINNING! Each lawyer brings a distinct skill set that enriches and extends our firm's ability to meet client needs. No client trusts important elements of a case to an inexperienced associate at Callahan & Blaine, simply because we don't have any.</p>
+					
+				</div><!-- sec_four_sp_right -->
+				
+			</div><!-- sec_four_sp_inner -->
+			
+		</div><!-- sec_four_selling_point -->
+		
+		<div class="sec_four_selling_point">
+			
+			<span class="number">02</span>
+			
+			<div class="sec_four_sp_inner">
+				
+				<div class="sec_four_sp_left">
+					
+					<span>The benefits of our integrated approach to accident litigation</span>
+					
+				</div><!-- sec_four_sp_left -->
+				
+				<div class="sec_four_sp_right content">
+					
+					<p>Our personal injury clients benefit from the breadth and depth of our experience in many fields: negligence law, insurance law, products liability, and the highly technical issues that can arise in the proof of damages in a brain injury case or a disabling accident involving a child. We're not only able to make the proof of complex damages claims stand up in the face of highly skilled opposition, we also know how to find the insurance coverage necessary to support the damages we can prove.</p>
+					
+				</div><!-- sec_four_sp_right -->
+				
+			</div><!-- sec_four_sp_inner -->
+			
+		</div><!-- sec_four_selling_point -->
 		
 	</div><!-- sec_four_selling_points -->
 	
