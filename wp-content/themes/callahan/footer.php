4bf4542cc0dac0ab8d8ff5a23b63b79969cbd9a6
Garrett Cullen <Garth619@users.noreply.github.com>
2019-11-22 18:41:19 -0800

    push

--- wp-content/themes/callahan/footer.php
+++ wp-content/themes/callahan/footer.php
@@ -4,14 +4,18 @@
 		
 		<div class="form_inner">
 			
+			<span class="free_consultation_title">Free Consultation</span><!-- free_consultation_title -->
 			
+			<span class="free_consultation_subtitle">Fill out the form below to receive a free initial consultation.</span><!-- free_consultation_subtitle -->
+			
+			<span class="required">*Field Required</span><!-- required -->
+			
+			<?php gravity_form(1, false, false, false, '', true, 12); ?>
 			
 		</div><!-- form_inner -->
 		
 	</div><!-- form_wrapper -->
 	
-	<?php // gravity_form(1, false, false, false, '', true, 12); ?>
-		
 </footer>
 
 
