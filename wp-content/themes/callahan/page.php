4959022b11da263e5836a3129ef7cb0759b6f97e
Garrett Cullen <Garth619@users.noreply.github.com>
2019-11-26 12:15:15 -0800

    push

--- wp-content/themes/callahan/page.php
+++ wp-content/themes/callahan/page.php
@@ -1,6 +1,18 @@
 <?php get_header(); ?>
 
-
+	<div class="internal_banner">
+		
+		<div class="internal_banner_content">
+			
+			<span class="internal_banner_header">California’s Premier Litigation Law Attorneys</span><!-- internal_banner_header -->
+			
+			<a class="banner_button" href="">Free Consultation</a><!-- banner_button -->
+			
+		</div><!-- internal_banner_content -->
+		
+		<img src="<?php bloginfo('template_directory');?>/images/int-hero-desktop.jpg"/>
+		
+	</div><!-- internal_banner -->
 		
 	<?php get_template_part( 'loop', 'page' ); ?>
 		
