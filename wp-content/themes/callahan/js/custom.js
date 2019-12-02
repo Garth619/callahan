109027010e169367f6688514c94b65b42d205735
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-02 11:42:21 -0800

    push

--- wp-content/themes/callahan/js/custom.js
+++ wp-content/themes/callahan/js/custom.js
@@ -403,31 +403,31 @@ $("ul > li.menu-item-has-children > a[href='#']").removeAttr("href");
 	// sidebar
 	
 		
-		$('<span class="h3_icon"></span>').insertAfter('.sidebarbox_inner h3'); 
+		$('<span class="h3_icon"></span>').insertAfter('.sidebarbox_inner h3, .widget h3'); 
 	 
 	 
-	 $('<span class="h3_icon"></span>').appendTo('.sidebarbox_inner ul.menu > li.menu-item-has-children > a');
+	 $('<span class="h3_icon"></span>').appendTo('.sidebarbox_inner ul.menu > li.menu-item-has-children > a, .widget > ul > li.menu-item-has-children > a');
 	 
-	 $('.sidebarbox_inner ul.menu > li.current-menu-ancestor > a').addClass('active');
+	 $('.sidebarbox_inner ul.menu > li.current-menu-ancestor > a, .widget > ul > li.current-menu-ancestor > a').addClass('active');
 
 
-	 $('.sidebarbox_inner ul.menu > li.menu-item-has-children > a').on('click', function(e) {
+	 $('.sidebarbox_inner ul.menu > li.menu-item-has-children > a, .widget > ul > li.menu-item-has-children > a').on('click', function(e) {
 		 
 		$(this).toggleClass('active');
 	   
-	 	$(this).next('ul.sub-menu').slideToggle(300);
+	 	$(this).next('ul').slideToggle(300);
 	 
 	 });
 	 
-	 $('.sidebar_box, .sidebarbox_inner > .widget > span.h3_icon').addClass('active');
+	 $('.sidebar_box, widget, .widget > span.h3_icon').addClass('active');//
 	 
-	 $('.sidebarbox_inner h3').on('click', function(e) {
+	 $('.sidebarbox_inner h3, .widget h3').on('click', function(e) {
 		 
-		 	$(this).parents('.sidebar_box').toggleClass('active');
+		 	$(this).parents('.sidebar_box, .widget').toggleClass('active');
 			
 			$(this).next().toggleClass('active');
 		  
-			$(this).siblings('ul.menu').slideToggle(300);
+			$(this).siblings('ul').slideToggle(300);
 			
 			
 		
