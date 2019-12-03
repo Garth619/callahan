2197f826b6389357820ea8928404576c466827fa
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-03 14:03:23 -0800

    push

--- wp-content/themes/callahan/page-templates/template-testimonials.php
+++ wp-content/themes/callahan/page-templates/template-testimonials.php
@@ -15,100 +15,69 @@ get_header(); ?>
 			
 			<div class="testi_col">
 				
-				<div class="single_testi">
+				<?php if(get_field('testimonial_column_one')): ?>
+				 
+					<?php while(has_sub_field('testimonial_column_one')): ?>
+				 
+						<div class="single_testi">
 					
-					<div class="single_testi_inner">
+							<div class="single_testi_inner">
 						
-						<img src="<?php bloginfo('template_directory');?>/images/test-stars-03.svg"/>
+								<img src="<?php bloginfo('template_directory');?>/images/test-stars-03.svg"/>
 						
-						<span class="tesi_quote">“Nam eu lectus duis scelerisque, bibendum sem”</span><!-- tesi_quote -->
+								<span class="tesi_quote"><?php the_sub_field( 'quote' ); ?></span><!-- tesi_quote -->
 						
-						<div class="content">
+								<div class="content">
 						
-							<p>Fusce in metus odio. Curabitur maximus efficitur purus, ac tincidunt lorem feugiat eget. Vestibulum varius mi at gravida maximus. Aliquam luctus arcu eros, eu semper felis egestas nec. Phasellus elementum ullamcorper felis, at bibendum libero tincidunt in. Nullam a nunc metus. Fusce placerat eget ante ut eleifend. Phasellus et molestie risus. Proin fringilla urna ut pulvinar rhoncus. Nunc sit amet varius tortor. </p>
+									<?php the_sub_field( 'content' ); ?>
 						
-							<p>Fusce in metus odio. Curabitur maximus efficitur purus, ac tincidunt lorem feugiat eget. Vestibulum varius mi at gravida maximus. Aliquam luctus arcu eros, eu semper felis egestas nec. Phasellus elementum ullamcorper felis, at bibendum libero tincidunt in. Nullam a nunc metus. Fusce placerat eget ante ut eleifend. Phasellus et molestie risus. Proin fringilla urna ut pulvinar rhoncus. Nunc sit amet varius tortor. </p>
+								</div><!-- content -->
 						
-						</div><!-- content -->
-						
-						<span class="testi_name">- John Doe</span><!-- testi_name -->
+						<span class="testi_name">- <?php the_sub_field( 'name' ); ?></span><!-- testi_name -->
 						
 					</div><!-- single_testi_inner -->
 					
 				</div><!-- single_testi -->
-				
-				<div class="single_testi">
-					
-					<div class="single_testi_inner">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/test-stars-03.svg"/>
-						
-						<span class="tesi_quote">“Nam eu lectus duis scelerisque, bibendum sem”</span><!-- tesi_quote -->
-						
-						<div class="content">
-						
-							<p>Fusce in metus odio. Curabitur maximus efficitur purus, ac tincidunt lorem feugiat eget. Vestibulum varius mi at gravida maximus. Aliquam luctus arcu eros, eu semper felis egestas nec. Phasellus elementum ullamcorper felis, at bibendum libero tincidunt in. Nullam a nunc metus. Fusce placerat eget ante ut eleifend. Phasellus et molestie risus. Proin fringilla urna ut pulvinar rhoncus. Nunc sit amet varius tortor. </p>
-						
-							<p>Fusce in metus odio. Curabitur maximus efficitur purus, ac tincidunt lorem feugiat eget. Vestibulum varius mi at gravida maximus. Aliquam luctus arcu eros, eu semper felis egestas nec. Phasellus elementum ullamcorper felis, at bibendum libero tincidunt in. Nullam a nunc metus. Fusce placerat eget ante ut eleifend. Phasellus et molestie risus. Proin fringilla urna ut pulvinar rhoncus. Nunc sit amet varius tortor. </p>
-						
-						</div><!-- content -->
-						
-						<span class="testi_name">- John Doe</span><!-- testi_name -->
-						
-					</div><!-- single_testi_inner -->
-					
-				</div><!-- single_testi -->
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
 				
 			</div><!-- testi_col -->
 			
 			<div class="testi_col">
 				
-				<div class="single_testi">
+				<?php if(get_field('testimonial_column_two')): ?>
+				 
+					<?php while(has_sub_field('testimonial_column_two')): ?>
+				 
+						<div class="single_testi">
 					
-					<div class="single_testi_inner">
+							<div class="single_testi_inner">
 						
-						<img src="<?php bloginfo('template_directory');?>/images/test-stars-03.svg"/>
+								<img src="<?php bloginfo('template_directory');?>/images/test-stars-03.svg"/>
 						
-						<span class="tesi_quote">“Nam eu lectus duis scelerisque, bibendum sem”</span><!-- tesi_quote -->
+								<span class="tesi_quote"><?php the_sub_field( 'quote' ); ?></span><!-- tesi_quote -->
 						
-						<div class="content">
+								<div class="content">
 						
-							<p>Fusce in metus odio. Curabitur maximus efficitur purus, ac tincidunt lorem feugiat eget. Vestibulum varius mi at gravida maximus. Aliquam luctus arcu eros, eu semper felis egestas nec. Phasellus elementum ullamcorper felis, at bibendum libero tincidunt in. Nullam a nunc metus. Fusce placerat eget ante ut eleifend. Phasellus et molestie risus. Proin fringilla urna ut pulvinar rhoncus. Nunc sit amet varius tortor. </p>
+									<?php the_sub_field( 'content' ); ?>
 						
-							<p>Fusce in metus odio. Curabitur maximus efficitur purus, ac tincidunt lorem feugiat eget. Vestibulum varius mi at gravida maximus. Aliquam luctus arcu eros, eu semper felis egestas nec. Phasellus elementum ullamcorper felis, at bibendum libero tincidunt in. Nullam a nunc metus. Fusce placerat eget ante ut eleifend. Phasellus et molestie risus. Proin fringilla urna ut pulvinar rhoncus. Nunc sit amet varius tortor. </p>
+								</div><!-- content -->
 						
-						</div><!-- content -->
-						
-						<span class="testi_name">- John Doe</span><!-- testi_name -->
+						<span class="testi_name">- <?php the_sub_field( 'name' ); ?></span><!-- testi_name -->
 						
 					</div><!-- single_testi_inner -->
 					
 				</div><!-- single_testi -->
-				
-				<div class="single_testi">
-					
-					<div class="single_testi_inner">
-						
-						<img src="<?php bloginfo('template_directory');?>/images/test-stars-03.svg"/>
-						
-						<span class="tesi_quote">“Nam eu lectus duis scelerisque, bibendum sem”</span><!-- tesi_quote -->
-						
-						<div class="content">
-						
-							<p>Fusce in metus odio. Curabitur maximus efficitur purus, ac tincidunt lorem feugiat eget. Vestibulum varius mi at gravida maximus. Aliquam luctus arcu eros, eu semper felis egestas nec. Phasellus elementum ullamcorper felis, at bibendum libero tincidunt in. Nullam a nunc metus. Fusce placerat eget ante ut eleifend. Phasellus et molestie risus. Proin fringilla urna ut pulvinar rhoncus. Nunc sit amet varius tortor. </p>
-						
-							<p>Fusce in metus odio. Curabitur maximus efficitur purus, ac tincidunt lorem feugiat eget. Vestibulum varius mi at gravida maximus. Aliquam luctus arcu eros, eu semper felis egestas nec. Phasellus elementum ullamcorper felis, at bibendum libero tincidunt in. Nullam a nunc metus. Fusce placerat eget ante ut eleifend. Phasellus et molestie risus. Proin fringilla urna ut pulvinar rhoncus. Nunc sit amet varius tortor. </p>
-						
-						</div><!-- content -->
-						
-						<span class="testi_name">- John Doe</span><!-- testi_name -->
-						
-					</div><!-- single_testi_inner -->
-					
-				</div><!-- single_testi -->
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
 				
 			</div><!-- testi_col -->
 			
+						
 		</div><!-- testimonial_page_wrapper -->
 			
 	</div><!-- container -->
