1cf89db16b98d21edd4b9163d131138b9d7bb887
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-04 14:53:47 -0800

    push

---
+++ wp-content/themes/callahan/sidebar-attbio.php
@@ -0,0 +1,35 @@
+
+<div class="sidebar_wrapper att_bio">
+	
+	<div class="sidebar_box">
+		
+		<div class="sidebarbox_inner">
+			
+			<div class="widget">
+				
+				<h3>Areas of Practice</h3>
+				
+				<ul>
+					<li>Test
+						<ul>
+							<li>test</li>
+							<li>test</li>
+							<li>test</li>
+							<li>test</li>
+						</ul>
+					</li>
+					<li>Test</li>
+					<li>Test</li>
+					<li>Test</li>
+					<li>Test</li>
+				</ul>
+				
+			</div><!-- widget -->
+			
+			<?php// bulk_sidebar(); ?>
+		
+		</div><!-- sidebarbox_inner -->
+		
+	</div><!-- sidebar_box -->
+	
+</div><!-- sidebar_wrapper -->
