bcc799a2a3d85af79a0914b803ce068591d873df
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-03 16:51:31 -0800

    push

--- wp-content/themes/callahan/page-templates/template-caseresults.php
+++ wp-content/themes/callahan/page-templates/template-caseresults.php
@@ -15,51 +15,105 @@ get_header(); ?>
 			
 			<div class="single_cr">
 				
-				
-				
-			</div><!-- single_cr -->
-			
-			<div class="single_cr">
-				
-				
-				
-			</div><!-- single_cr -->
-			
-			<div class="single_cr">
-				
-				
+				<div class="single_cr_inner">
+					
+					<span class="amount">$47m</span><!-- amount -->
+					
+					<span class="cr_content">Jury clears widow in any wrongdoing in Ponzi Scheme</span><!-- cr_content -->
+					
+				</div><!-- single_cr_inner -->
 				
 			</div><!-- single_cr -->
 			
 			<div class="single_cr">
 				
-				
-				
-			</div><!-- single_cr -->
-			
-			<div class="single_cr">
-				
-				
-				
-			</div><!-- single_cr -->
-			
-			<div class="single_cr">
-				
-				
+				<div class="single_cr_inner">
+					
+					<span class="amount">$934m</span><!-- amount -->
+					
+					<span class="cr_content">Trial of complex business dispute largest jury verdict in Orange County history</span><!-- cr_content -->
+					
+				</div><!-- single_cr_inner -->
 				
 			</div><!-- single_cr -->
 			
 			<div class="single_cr">
 				
-				
+				<div class="single_cr_inner">
+					
+					<span class="amount">$45m</span><!-- amount -->
+					
+					<span class="cr_content">Fortune 500 Company, Mitsubishi Caterpillar Forklift America, Inc., was led to&nbsp;victory </span><!-- cr_content -->
+					
+				</div><!-- single_cr_inner -->
 				
 			</div><!-- single_cr -->
 			
 			<div class="single_cr">
 				
-				
+				<div class="single_cr_inner">
+					
+					<span class="amount">$120m</span><!-- amount -->
+					
+					<span class="cr_content">Pellentesque sit amet nibh fermentum, laoreet nibh in, volutpat nulla</span><!-- cr_content -->
+					
+				</div><!-- single_cr_inner -->
 				
 			</div><!-- single_cr -->
+			
+			<div class="single_cr">
+				
+				<div class="single_cr_inner">
+					
+					<span class="amount">$120m</span><!-- amount -->
+					
+					<span class="cr_content">Pellentesque sit amet nibh fermentum, laoreet nibh in, volutpat nulla</span><!-- cr_content -->
+					
+				</div><!-- single_cr_inner -->
+				
+			</div><!-- single_cr -->
+
+			
+			<div class="single_cr">
+				
+				<div class="single_cr_inner">
+					
+					<span class="amount">$120m</span><!-- amount -->
+					
+					<span class="cr_content">Pellentesque sit amet nibh fermentum, laoreet nibh in, volutpat nulla</span><!-- cr_content -->
+					
+				</div><!-- single_cr_inner -->
+				
+			</div><!-- single_cr -->
+
+			
+			<div class="single_cr">
+				
+				<div class="single_cr_inner">
+					
+					<span class="amount">$120m</span><!-- amount -->
+					
+					<span class="cr_content">Pellentesque sit amet nibh fermentum, laoreet nibh in, volutpat nulla</span><!-- cr_content -->
+					
+				</div><!-- single_cr_inner -->
+				
+			</div><!-- single_cr -->
+
+			
+			<div class="single_cr">
+				
+				<div class="single_cr_inner">
+					
+					<span class="amount">$120m</span><!-- amount -->
+					
+					<span class="cr_content">Pellentesque sit amet nibh fermentum, laoreet nibh in, volutpat nulla</span><!-- cr_content -->
+					
+				</div><!-- single_cr_inner -->
+				
+			</div><!-- single_cr -->
+
+			
+			
 			
 		</div><!-- cr_wrapper -->
 			
