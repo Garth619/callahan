92ef86b84b4cd865b69d308b4f31ea53413116a2
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-04 14:29:38 -0800

    push

--- wp-content/themes/callahan/page-templates/template-bio.php
+++ wp-content/themes/callahan/page-templates/template-bio.php
@@ -4,9 +4,41 @@
 
 get_header(); ?>
 
-
-
-	<?php get_template_part( 'loop', 'page' ); ?>
+<div id="internal_main">
+	
+	<div class="container">
+	
+		<div class="att_bio_top">
+			
+			<div class="att_top_inner">
+			
+				<div class="att_bio_left">
+				
+					<img class="att_bio_img" src="<?php bloginfo('template_directory');?>/images/att-bio-callahan.jpg"/><!-- att_bio_img -->
+				
+				</div><!-- att_bio_left -->
+			
+				<div class="att_bio_right">
+					
+					<h1 class="att_bio_header" ><?php the_title();?></h1><!-- att_bio_header -->
+					
+					<span class="position">Founding Partner</span><!-- position -->
+					
+					<div class="att_top_content">
+						
+						<p>Daniel Callahan has <a href="">distinguished</a> himself as one of the top trial attorneys in California, and has repeatedly been recognized by his peers for his incredible accomplishments.  Mr. Callahan was the winner of the prestigious OCTLA Trial Lawyer of the Year Award three times, in 2000, 2004, and 2012.</p>
+						
+					</div><!-- att_top_content -->
+					
+				</div><!-- att_bio_right -->
+			
+			</div><!-- att_top_inner -->
+			
+		</div><!-- att_bio_top -->
+		
+	</div><!-- container -->
+		
+</div><!-- internal_main -->
 					 
 					 	
 <?php get_footer(); ?>
