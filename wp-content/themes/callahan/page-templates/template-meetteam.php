b940f9a1bc834ca4c7e7fe8e7cde229976c4217f
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-04 18:52:24 -0800

    push

--- wp-content/themes/callahan/page-templates/template-meetteam.php
+++ wp-content/themes/callahan/page-templates/template-meetteam.php
@@ -12,46 +12,63 @@ get_header(); ?>
 			
 		<div class="att_wrapper">
 			
-			<div class="single_att">
+			<?php if(get_field('attorneys')): ?>
+			 
+				<?php while(has_sub_field('attorneys')): ?>
+			 
+						<?php $post_object = get_sub_field( 'attorney' ); ?>
+						
+						<?php if ( $post_object ): ?>
+							
+							<?php $post = $post_object; ?>
+							
+							<?php setup_postdata( $post ); ?> 
 				
-				<a class="" href="">
 				
-				<div class="single_att_img_wrapper">
+								<div class="single_att">
+				
+									<a class="" href="<?php the_permalink();?>">
+				
+										<div class="single_att_img_wrapper">
 					
-					<div class="single_att_inner">
+											<div class="single_att_inner">
 						
-						<div class="single_profile_pic_wrapper">
+												<div class="single_profile_pic_wrapper">
+													
+													<?php $attorney_profile_image = get_field( 'attorney_profile_image' ); ?>
+													
+													<?php if ( $attorney_profile_image ) { ?>
+														
+														<img class="profile_pic" src="<?php echo $attorney_profile_image['url']; ?>" alt="<?php echo $attorney_profile_image['alt']; ?>" />
+													
+													<?php } ?>
 						
-							<img class="profile_pic" src="<?php bloginfo('template_directory');?>/images/att-bio-callahan.jpg"/><!-- profile_pic -->
+													<div class="single_att_overlay"></div><!-- single_att_overlay -->
 						
-							<div class="single_att_overlay"></div><!-- single_att_overlay -->
+												</div><!-- single_profile_pic_wrapper -->
 						
-						</div><!-- single_profile_pic_wrapper -->
-						
-					</div><!-- single_att_inner -->
+											</div><!-- single_att_inner -->
 					
-					<div class="single_overlay_hover"></div><!-- single_overlay_hover -->
+											<div class="single_overlay_hover"></div><!-- single_overlay_hover -->
 					
-				</div><!-- single_att_img_wrapper -->
+										</div><!-- single_att_img_wrapper -->
 				
-				<span class="att_title">Daniel J. Callahan</span><!-- att_title -->
+										<span class="att_title"><?php the_title();?></span><!-- att_title -->
 				
-				<span class="att_position">Founding Partner</span><!-- att_position -->
+										<span class="att_position"><?php the_field( 'position' ); ?></span><!-- att_position -->
 				
+									</a>
 				
+								</div><!-- single_att -->
 				
-				</a>
-				
-			</div><!-- single_att -->
-			
-			<div class="single_att"></div><!-- single_att -->
-			
-			<div class="single_att"></div><!-- single_att -->
-			
-			<div class="single_att"></div><!-- single_att -->
-			
-			<div class="single_att"></div><!-- single_att -->
-			
+							<?php wp_reset_postdata(); ?>
+								
+						<?php endif; ?>
+			    
+				<?php endwhile; ?>
+			 
+			<?php endif; ?>
+						
 		</div><!-- att_wrapper -->
 			
 	</div><!-- container -->
