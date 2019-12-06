635484b68c946e62ca72e094c81fd1069895a632
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-05 20:19:38 -0800

    push

--- wp-content/themes/callahan/page-templates/template-about.php
+++ wp-content/themes/callahan/page-templates/template-about.php
@@ -16,7 +16,7 @@ get_header(); ?>
 			 
 			 <div class="about_intro_wrapper">
 				 
-				 <p>Sed aliquam lectus neque, eget tincidunt justo bibendum nec. Aenean pharetra sodales augue sit amet iaculis. Praesent ultricies sollicitudin lectus, ac cursus odio sodales porttitor.</p>
+				 <p>Sed aliquam lectus neque, eget tincidunt justo bibendum nec. Aenean pharetra sodales augue sit amet iaculis. Praesent ultricies sollicitudin lectus, ac cursus odio sodales porttitor.</p>
 				 
 			 </div><!-- about_intro_wrapper -->
 			 
@@ -34,7 +34,27 @@ get_header(); ?>
 			
 			<div class="about_right">
 				
-				
+				<div class="about_video_wrapper">
+					
+					<a class="" href="https://www.youtube.com/embed/XBPjVzSoepo" data-lity>
+					
+					<div class="about_video_thumb">
+						
+						<img class="about_thumb" src="<?php bloginfo('template_directory');?>/images/int-about-video-thumb.jpg"/><!-- about_thumb -->
+						
+						<div class="about_video_overlay">
+							
+							<?php echo file_get_contents("wp-content/themes/callahan/images/icon-play-button.svg"); ?>
+							
+						</div><!-- about_video_overlay -->
+						
+					</div><!-- about_video_thumb -->
+					
+					<span class="about_video_description">Praesent tempor est vel neque rutrum, in tempor nibh ultricies. Morbi scelerisque set accumsan pharetra odio neque.</span><!-- about_video_description -->
+					
+					</a>
+					
+				</div><!-- about_video_wrapper -->
 				
 			</div><!-- about_right -->
 			
