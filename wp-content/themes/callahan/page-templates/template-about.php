3052937598898d6fc1be6e1ecbac45f7d4df8201
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-06 13:18:33 -0800

    push

--- wp-content/themes/callahan/page-templates/template-about.php
+++ wp-content/themes/callahan/page-templates/template-about.php
@@ -12,6 +12,10 @@ get_header(); ?>
 			
 			<div class="about_left">
 				
+				<div class="about_left_inner">
+					
+					<div class="about_left_content">
+				
 			 <h1 class="about_header"><?php the_title();?></h1><!-- about_header -->
 			 
 			 <div class="about_intro_wrapper">
@@ -19,6 +23,32 @@ get_header(); ?>
 				 <p>Sed aliquam lectus neque, eget tincidunt justo bibendum nec. Aenean pharetra sodales augue sit amet iaculis. Praesent ultricies sollicitudin lectus, ac cursus odio sodales porttitor.</p>
 				 
 			 </div><!-- about_intro_wrapper -->
+			 
+			 </div><!-- about_left_content -->
+			 
+			 <div class="about_video_wrapper">
+					
+					<a class="" href="https://www.youtube.com/embed/XBPjVzSoepo" data-lity>
+					
+					<div class="about_video_thumb">
+						
+						<img class="about_thumb" src="<?php bloginfo('template_directory');?>/images/int-about-video-thumb.jpg"/><!-- about_thumb -->
+						
+						<div class="about_video_overlay">
+							
+							<?php echo file_get_contents("wp-content/themes/callahan/images/icon-play-button.svg"); ?>
+							
+						</div><!-- about_video_overlay -->
+						
+					</div><!-- about_video_thumb -->
+					
+					<span class="about_video_description">Praesent tempor est vel neque rutrum, in tempor nibh ultricies. Morbi scelerisque set accumsan pharetra odio neque.</span><!-- about_video_description -->
+					
+					</a>
+					
+				</div><!-- about_video_wrapper -->
+				
+				</div><!-- about_left_inner -->
 			 
 			 <div class="about_top_content content">
 				 
@@ -60,7 +90,55 @@ get_header(); ?>
 			
 		</div><!-- about_top_wrapper -->
 			
+		<div class="about_blockquote">
+			
+			<div class="about_blocquote_inner">
+				
+				<span class="about_blockquote_title">What We Stand For</span><!-- about_blockquote_title -->
+				
+				<div class="about_blockquote_content">
+					
+					<p>Our law firm represents corporate, professional and entrepreneurial clients of all sizes: multi-national, Fortune 500 companies, California-based corporations, medium and small businesses, homeowners associations, and employers and individuals engaged in business.</p>
+					
+				</div><!-- about_blockquote_content -->
+				
+			</div><!-- about_blocquote_inner -->
+			
+		</div><!-- about_blockquote -->
 		
+		<div class="about_bottom_wrapper content">
+			
+			<div class="about_col">
+				
+				<h2>Duis ut purus scelerisque, eleifend neque</h2>
+				
+				<p><strong>Sed aliquam lectus neque, eget tincidunt justo bibendum nec. Aenean pharetra sodales augue sit amet iaculis. Praesent ultricies sollicitudin lectus, ac cursus odio sodales porttitor. Suspendisse vel risus ac nulla malesuada vehicula:</strong></p>
+				
+				<ul>
+
+					<li>Duis dignissim, metus nec suscipit eleifend, orci dolor laoreet eros, nec finibus.</li>
+					<li>Nullam accumsan ex in metus semper, nec consectetur sapien venenatis. Donec tincidunt ornare volutpat.</li>
+					<li>Duis ut purus scelerisque, eleifend neque sed, pulvinar purus. Maecenas ut pellentesque elit. Proin ex eros, volutpat at consequat vitae.</li>
+					<li>Vestibulum luctus erat id rhoncus tincidunt. Donec porta odio nunc, eget semper turpis placerat nec. Vivamus pellentesque imperdiet ex, nec sollicitudin risus.</li>
+				
+				</ul>
+
+				</div><!-- about_col -->
+			
+			<div class="about_col">
+				
+				<ul>
+		
+				<li>Phasellus hendrerit erat id sem vestibulum eleifend. Mauris bibendum, sem sollicitudin vestibulum venenatis.</li>
+				
+				<li>Destibulum luctus erat id rhoncus tincidunt. Donec porta odio nunc, eget semper turpis placerat nec. Vivamus pellentesque imperdiet ex, nec sollicitudin risus rutrum quis. Cras mollis scelerisque dui, et interdum magna volutpat in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at nisi velit. Ut fringilla, mauris facilisis congue fermentum, orci nunc porta mi, quis viverra quam dui vel augue. Vestibulum id libero laoreet, vulputate velit a, accumsan velit. In tristique ante sed aliquet venenatis. Suspendisse volutpat accumsan viverra.</p>
+				
+				
+			</div><!-- about_col -->
+			
+		</div><!-- about_bottom_wrapper -->
 			
 	</div><!-- container -->
 		
